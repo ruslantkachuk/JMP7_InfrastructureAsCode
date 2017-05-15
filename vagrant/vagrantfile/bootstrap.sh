fdad8515fb937f0a35ab274d28c9688faf4a15bd
ruslantkachuk <Ruslan_Tkachuk@epam.com>
2017-05-16 00:22:06 +0300

    JMP-0005 Added quite mode for wget

--- vagrant/vagrantfile/bootstrap.sh
+++ vagrant/vagrantfile/bootstrap.sh
@@ -7,18 +7,22 @@ sudo yum -y install unzip
 sudo yum -y install git
 cd /opt/
 # Install JAVA 8
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
+echo "Install JAVA 8 ..."
+wget --quiet --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
 tar xzf jdk-8u131-linux-x64.tar.gz
 # Install Gradle 3.5
-wget https://services.gradle.org/distributions/gradle-3.5-all.zip
+echo "Install Gradle 3.5 ..."
+wget --quiet https://services.gradle.org/distributions/gradle-3.5-all.zip
 mkdir /opt/gradle
 unzip -d /opt/gradle gradle-3.5-all.zip
 # Install Tomcat 8
-wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.14/bin/apache-tomcat-8.5.14.tar.gz
+echo "Install Tomcat 8.5.14 ..."
+wget --quiet http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.14/bin/apache-tomcat-8.5.14.tar.gz
 tar xvf apache-tomcat-8.5.14.tar.gz
 mv apache-tomcat-8.5.14 tomcat
 # Download HSQLDB JDBC Driver
-wget https://sourceforge.net/projects/hsqldb/files/hsqldb/hsqldb_2_4/hsqldb-2.4.0.zip/download?use_mirror=pilotfiber -O hsqldb-2.4.0.zip
+echo "Install HSQLDB JDBC Driver ..."
+wget --quiet https://sourceforge.net/projects/hsqldb/files/hsqldb/hsqldb_2_4/hsqldb-2.4.0.zip/download?use_mirror=pilotfiber -O hsqldb-2.4.0.zip
 mkdir /opt/hsqldb
 unzip -d /opt/hsqldb hsqldb-2.4.0.zip
 cp /opt/hsqldb/hsqldb-2.4.0/hsqldb/lib/hsqldb.jar /opt/tomcat/lib
