e6ee011f716bd4771f6b9642a145c8d288520a17
ruslantkachuk <Ruslan_Tkachuk@epam.com>
2017-05-15 18:14:36 +0300

    JMP-0004 - Fix config

---
+++ docker/src/main/java/com/jmp/config/JmpApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.jmp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("com.jmp.repository")
+public class JmpApplicationConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+}
